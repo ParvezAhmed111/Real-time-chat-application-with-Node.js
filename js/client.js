e036cbecba81a444211f49a0cae557013c977351
Sheikh Parvez Ahmed <parvezahmed.0947@gmail.com>
2021-10-29 13:34:43 +0530

    Day2

--- js/client.js
+++ js/client.js
@@ -1,23 +1,42 @@
-const {io} = require("socket.io-client");
-const socket= io('http://localhost:5500');
+// const {io} = require("socket.io-client");
+const socket= io('http://localhost:8000');
 
 const form= document.getElementById('send-container');
 const messageInput= document.getElementById('messageInp');  
 const messageContainer= document.querySelector(".container");   //if msg comes, i will put it in container
+var audio= new Audio('../ting.mp3')
 
 
 const append= (message, position)=>{
     const messageElement = document.createElement('div');
     messageElement.innerText=message;
     messageElement.classList.add('message');
-    messageElement.classList.add('position');
+    messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    if(position=='left'){
+        audio.play();
+    }
 }
 
-const name = socket.prompt("Enter you name to join: ");
-socket.emit('new-user-joined', name);
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message= messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value='';
+})
 
+// const name1 = prompt("Enter you name to join: ");
+socket.emit('new-user-joined', name1);
 
 socket.on('user-joined', name=>{
-    append(`${name} joined the chat`, name);
+    append(`${name} joined the chat`, 'center');
+})
+
+socket.on('recieve', data=>{
+    append(`${data.name}: ${data.message}`, 'left');
+})
+
+socket.on('left', name=>{
+    append(`${name} left the chat`, 'center');
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -2,19 +2,23 @@
 // const io= require('socket.io')(8000);    //8000 is port, you can take any port
 
 const { Server } = require("socket.io");
-
-const io = new Server(5500);
+const io = new Server(8000);
 
 const users={};
 
 io.on('connection', socket =>{
     socket.on('new-user-joined', name=>{
-        console.log("new user: ", name);
+        // console.log("new user: ", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message=>{
         socket.broadcast.emit('recieve', {message: message, name: users[socket.id]});
+    });
+
+    socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
     });
 });
